f17742a90130a34ffebec4805955127642ff9d69
Julien <gerard.julien2000@orange.fr>
2022-01-06 11:25:21 +0100

    front and backend complete

---
+++ build/precache-manifest.bea849e9e03eb15cb0b752d093f569ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab0384d71be5a6deeb88",
+    "url": "/static/css/main.1a2c8b16.chunk.css"
+  },
+  {
+    "revision": "ab0384d71be5a6deeb88",
+    "url": "/static/js/main.ad11ca63.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2fee9a003f490e8ad47c",
+    "url": "/static/js/2.c64efb7f.chunk.js"
+  },
+  {
+    "revision": "65c7535f92ae179bfc7b1facdba2df16",
+    "url": "/index.html"
+  }
+];
